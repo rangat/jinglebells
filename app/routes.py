c7aa82f0cde73b90dcbd1e9e200e3af6f1bb2f2d
Rangaraj Tirumala <rangaraj.t@gmail.com>
2018-09-11 22:01:17 -0400

    cleaned up routing

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,11 @@ def index():
     # Baseline intro screen
     return "Hello, World!"
 
-@app.route('/santa/<int:santa_id>')
+@app.route('/santa/<int:santa_id>', methods=['GET'])
 def santa(santa_id):
     # Get santa_id and find data to fill in app
+    new_user = request.args.get('new_user')
+    if  new_user and new_user.isdigit() and int(new_user)==1:
+        # This link adds new users to santa_id
+        return "New User"
     return str(santa_id)

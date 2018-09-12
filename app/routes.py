29638c97660b5e8c437d60732d08ea9a27e517c2
Rangaraj Tirumala <rangaraj.t@gmail.com>
2018-09-11 21:29:05 -0400

    tried to figure out get request

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
+from flask import request
 
 @app.route('/')
 @app.route('/index')
 def index():
+    # Baseline intro screen
     return "Hello, World!"
+
+@app.route('/santa', methods=['GET'])
+def santa():
+    return request.data
